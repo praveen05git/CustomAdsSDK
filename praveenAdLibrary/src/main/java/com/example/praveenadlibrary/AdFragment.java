90ac834fd773071978bbc9a01312363f75a957ab
praveen <praveen051997@gmail.com>
2020-10-04 12:33:24 +0530

    Initial

--- praveenAdLibrary/src/main/java/com/example/praveenadlibrary/AdFragment.java
+++ praveenAdLibrary/src/main/java/com/example/praveenadlibrary/AdFragment.java
@@ -2,11 +2,21 @@ package com.example.praveenadlibrary;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +33,10 @@ public class AdFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    static List<adProperties> adLists;
+    static FirebaseDatabase firebaseDatabase;
+    static DatabaseReference databaseReference;
 
     public AdFragment() {
         // Required empty public constructor
@@ -59,6 +73,37 @@ public class AdFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.ad_fragment, container, false);
+        View root=inflater.inflate(R.layout.ad_fragment, container, false);
+
+        adLists=new ArrayList<>();
+
+        firebaseDatabase= FirebaseDatabase.getInstance();
+
+        databaseReference = firebaseDatabase.getReference("adlist");
+
+
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot postSnapshot: snapshot.getChildren())
+                {
+
+                    adProperties ads= postSnapshot.getValue(adProperties.class);
+                    adLists.add(ads);
+
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+
+
+
+        return root;
     }
 }
